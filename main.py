1db16a9f7d8d478f5b0e54563a24bc323f5cb17d
Nasu Ni Furenaide <109841184+DNNDHH@users.noreply.github.com>
2026-01-06 13:53:00 +0800

    Update main.py

--- main.py
+++ main.py
@@ -13,28 +13,67 @@ s = NewSession()
 
 
 url_A = os.environ.get('URL_A')
-headers_A = os.environ.get('HEADERS_A')
-res = s.get(url_A, verify=False)
+headers_A = {
+  'User-Agent': "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/143.0.0.0 Safari/537.36",
+  'Accept': "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7",
+  'Accept-Encoding': "gzip, deflate, br, zstd",
+  'cache-control': "max-age=0",
+  'sec-ch-ua': "\"Google Chrome\";v=\"143\", \"Chromium\";v=\"143\", \"Not A(Brand\";v=\"24\"",
+  'sec-ch-ua-mobile': "?0",
+  'sec-ch-ua-platform': "\"Windows\"",
+}
+res = s.get(url_A, headers=headers_A, verify=False)
 _csrf = s.cookies.get("_csrf")
 print("1")
 
 
 info = os.environ.get('INFO')
-payload = f"_csrf={_csrf}&{str(info)}"
-headers_B = os.environ.get('HEADERS_B')
-res2 = s.post(url_A, data=payload, verify=False)
+payload = f"_csrf={_csrf}&{info}"
+headers_B = {
+  'User-Agent': "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/143.0.0.0 Safari/537.36",
+  'Accept': "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7",
+  'Accept-Encoding': "gzip, deflate, br, zstd",
+  'Content-Type': "application/x-www-form-urlencoded",
+  'cache-control': "max-age=0",
+  'sec-ch-ua': "\"Google Chrome\";v=\"143\", \"Chromium\";v=\"143\", \"Not A(Brand\";v=\"24\"",
+  'sec-ch-ua-mobile': "?0",
+  'sec-ch-ua-platform': "\"Windows\"",
+}
+res2 = s.post(url_A, data=payload, headers=headers_B, verify=False)
 print("2")
 
 
 url_B = os.environ.get('URL_B')
-headers_C = os.environ.get('HEADERS_C')
-response3 = s.get(url_B, verify=False)
+headers_C = {
+  'User-Agent': "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/143.0.0.0 Safari/537.36",
+  'Accept': "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7",
+  'Accept-Encoding': "gzip, deflate, br, zstd",
+  'cache-control': "max-age=0",
+  'dnt': "1",
+  'upgrade-insecure-requests': "1",
+  'sec-fetch-site': "same-origin",
+  'sec-fetch-mode': "navigate",
+  'sec-fetch-user': "?1",
+  'sec-fetch-dest': "document",
+  'sec-ch-ua': "\"Google Chrome\";v=\"143\", \"Chromium\";v=\"143\", \"Not A(Brand\";v=\"24\"",
+  'sec-ch-ua-mobile': "?0",
+  'sec-ch-ua-platform': "\"Windows\"",
+  'priority': "u=0, i",
+}
+response3 = s.get(url_B, headers=headers_C, verify=False)
 print("3")
 
 
 url_C = os.environ.get('URL_C')
-headers_D = os.environ.get('HEADERS_D')
-res = s.get(url_C, verify=False)
+headers_D = {
+  'User-Agent': "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/143.0.0.0 Safari/537.36",
+  'Accept': "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7",
+  'Accept-Encoding': "gzip, deflate, br, zstd",
+  'sec-ch-ua': "\"Google Chrome\";v=\"143\", \"Chromium\";v=\"143\", \"Not A(Brand\";v=\"24\"",
+  'sec-ch-ua-mobile': "?0",
+  'sec-ch-ua-platform': "\"Windows\"",
+}
+res = s.get(url_C, headers=headers_D, verify=False)
 data = res.json()
 
 with open("update.json", 'w', encoding='utf-8') as f:
