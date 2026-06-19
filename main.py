99fa3b4d3aa3deeef1705a6eff6ebb73e0a19b96
Nasu Ni Furenaide <109841184+DNNDHH@users.noreply.github.com>
2026-06-19 13:57:23 +0800

    Update main.py

--- main.py
+++ main.py
@@ -65,7 +65,6 @@ print("3")
 """
 
 url_C = os.environ.get('URL_C')
-"""
 headers_D = {
   'User-Agent': "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/143.0.0.0 Safari/537.36",
   'Accept': "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7",
@@ -79,24 +78,15 @@ data = res.json()
 
 with open("update.json", 'w', encoding='utf-8') as f:
     json.dump(data, f, ensure_ascii=False, indent=2)
-"""
 print("4")
-print(f"{url_C}")
 
 
 url_D = os.environ.get('URL_D')
-"""
 res = s.get(url_D, headers=headers_D, verify=False)
 data = res.json()
 
 with open("Shopdate.json", 'w', encoding='utf-8') as f:
     json.dump(data, f, ensure_ascii=False, indent=2)
-"""
 print("5")
-print(f"{url_D}")
 
-data = (f"{url_C}+{url_D}")
-print(f"{data}")
-with open("tst.json", 'w', encoding='utf-8') as f:
-    json.dump(data, f, ensure_ascii=False, indent=2)
 
